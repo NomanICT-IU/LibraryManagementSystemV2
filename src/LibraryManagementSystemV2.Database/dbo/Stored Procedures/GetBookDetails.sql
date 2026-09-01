d2b33e5d62359478eba84d48cf20e2ace94cb219
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-09-01 23:01:39 +0600

    refactor getBookDetails

--- src/LibraryManagementSystemV2.Database/dbo/Stored Procedures/GetBookDetails.sql
+++ src/LibraryManagementSystemV2.Database/dbo/Stored Procedures/GetBookDetails.sql
@@ -1,18 +1,20 @@
-﻿Create      PROCEDURE [dbo].[GetBookDetails]
-    @SearchBy     NVARCHAR(20),
+﻿CREATE   PROCEDURE dbo.GetBookDetails
+    @SearchBy   NVARCHAR(20),
     @SearchText NVARCHAR(100)
 AS
 BEGIN
+    
 
-  
+    -- 1. Store searched books in temp table
     SELECT
-    b.BookId,
+        b.BookId,
         b.Title,
         b.Author,
         b.ISBN,
         b.Category
-    FROM [dbo].[Book] AS b
-      WHERE
+    INTO #TempBooks
+    FROM dbo.Book AS b
+    WHERE
         (@SearchBy = 'Title'
             AND b.Title LIKE '%' + @SearchText + '%')
         OR
@@ -23,51 +25,49 @@ BEGIN
             AND b.ISBN LIKE '%' + @SearchText + '%');
 
 
+    -- 2. Book details
     SELECT
-  bc.BookId,
+        BookId,
+        Title,
+        Author,
+        ISBN,
+        Category
+    FROM #TempBooks;
+
+
+    -- 3. Book copy summary
+    SELECT
+        bc.BookId,
         COUNT(*) AS Total,
 
-        COUNT(
-            CASE
-                WHEN Status = 1 THEN 1
-            END
-        ) AS Available,
+        COUNT(CASE
+            WHEN bc.Status = 1 THEN 1
+        END) AS Available,
 
-        COUNT(
-            CASE
-                WHEN Status = 2 THEN 1
-            END
-        ) AS Borrowed,
+        COUNT(CASE
+            WHEN bc.Status = 2 THEN 1
+        END) AS Borrowed,
 
         CASE
-            WHEN COUNT(CASE WHEN Status = 1 THEN 1 END) > 0
+            WHEN COUNT(CASE WHEN bc.Status = 1 THEN 1 END) > 0
                 THEN 'Available'
 
-            WHEN COUNT(CASE WHEN Status = 2 THEN 1 END) > 0
+            WHEN COUNT(CASE WHEN bc.Status = 2 THEN 1 END) > 0
                 THEN 'Borrowed'
 
             ELSE 'Unknown'
         END AS Status
 
-    FROM [dbo].[BookCopy] as bc
-    join [dbo].[Book] as b
-     on bc.BookId = b.BookId
-  WHERE
-        (@SearchBy = 'Title'
-            AND b.Title LIKE '%' + @SearchText + '%')
-        OR
-        (@SearchBy = 'Author'
-            AND b.Author LIKE '%' + @SearchText + '%')
-        OR
-        (@SearchBy = 'ISBN'
-            AND b.ISBN LIKE '%' + @SearchText + '%')
-GROUP BY
-    bc.BookId
+    FROM dbo.BookCopy AS bc
+    INNER JOIN #TempBooks AS tb
+        ON bc.BookId = tb.BookId
 
-ORDER BY
-    bc.BookId;
+    GROUP BY bc.BookId
+
+    ORDER BY bc.BookId;
 
 
+    -- 4. Individual book copies
     SELECT
         bc.BookId,
         bc.CopyCode,
@@ -88,26 +88,16 @@ ORDER BY
             ELSE NULL
         END AS DueDate
 
-    FROM [dbo].[BookCopy] AS bc
-    join [dbo].[Book] as b
-     on bc.BookId = b.BookId
+    FROM dbo.BookCopy AS bc
 
-    LEFT JOIN [dbo].[BorrowRecord] AS br
+    INNER JOIN #TempBooks AS tb
+        ON bc.BookId = tb.BookId
+
+    LEFT JOIN dbo.BorrowRecord AS br
         ON bc.CopyId = br.CopyId
 
-    LEFT JOIN [dbo].[Member] AS m
+    LEFT JOIN dbo.Member AS m
         ON br.MemberId = m.MemberId
-
-     WHERE
-        (@SearchBy = 'Title'
-            AND b.Title LIKE '%' + @SearchText + '%')
-        OR
-        (@SearchBy = 'Author'
-            AND b.Author LIKE '%' + @SearchText + '%')
-        OR
-        (@SearchBy = 'ISBN'
-            AND b.ISBN LIKE '%' + @SearchText + '%')
-
 
     ORDER BY bc.CopyCode;
 
